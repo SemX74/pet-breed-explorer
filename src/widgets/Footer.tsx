03c973d2e0fa885faf5f04dfd801ac1deaad6aa6
Mykola Semenyuk <88790026+SemX74@users.noreply.github.com>
2024-11-05 22:56:36 +0200

    Update Footer.tsx

--- src/widgets/Footer.tsx
+++ src/widgets/Footer.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 
 const Footer: FC = () => {
-  return <footer className="mt-auto p-5 text-center">Made with ❤️ by Mykola Semenyuk. All rights reserved.</footer>;
+  return (
+    <footer className="mt-auto p-5 text-center">
+      Made with ❤️ by Mykola Semenyuk. All rights reserved.
+    </footer>
+  );
 };
 
 export default Footer;
